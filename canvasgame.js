bd4cc44652464e69fb079b62ee35e654a52e705f
Koroneiki <101889814+Koroneiki@users.noreply.github.com>
2023-02-08 12:35:52 +0100

    Add files via upload

--- canvasgame.js
+++ canvasgame.js
@@ -1,21 +1,51 @@
-  const canvas = document.getElementById("myCanvas");
-  const context = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
 
-  context.font = "30px Arial";
-  context.fillStyle = "blue";
-  context.strokeStyle = "red";
+const text_var = "\u26BD";
 
-  let x = 50;
-  let y = 50;
-  let velocity = {x: 5, y: 5};
+let textx = 50;
+let texty = 50;
+let vtextx = 6;
+let vtexty = 6;
+let atextx = 0.0;
+let atexty = 0.0;
 
-  function update() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    x += velocity.x;
-    y += velocity.y;
-    context.fillText("Hello, World!", x, y);
-    context.strokeText("Hello, World!", x, y);
-    requestAnimationFrame(update);
+context.font = "30px Arial";
+context.fillStyle = "blue";
+context.strokeStyle = "red";
+
+function moveText() {
+  textx += vtextx;
+  texty += vtexty;
+  vtextx += atextx;
+  vtexty += atexty;
+
+  // check for collision with bottom wall
+  if (texty + 28 >= canvas.height) {
+    vtexty = -vtexty;
   }
+  // check for collision with top wall
+  if (texty - 32 <= 0) {
+    vtexty = -vtexty;
+  }
+  
+  // check for collision with right wall
+  if (textx + context.measureText(text_var).width + 5 >= canvas.width) {
+    vtextx = -vtextx;
+  }
+  // check for collision with left wall
+  if (textx <= 0) {
+    vtextx = -vtextx;
+  }
+}
 
-  update();
+
+function generateFrame() {
+  moveText();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillText(text_var, textx, texty);
+  context.strokeText("", textx, texty);
+}
+
+var dt = 1000 / 25;
+setInterval(generateFrame, dt);
