2402f25a842e9328b1586d1e15c3ae47e2cdac36
Koroneiki <101889814+Koroneiki@users.noreply.github.com>
2024-02-13 11:40:47 +0100

    Add files via upload

--- leaflet_main/cities.js
+++ leaflet_main/cities.js
@@ -1,1 +1,60 @@
+function loadCities(map, cityName, callback) {
+    fetch('sorted_cities.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log('GeoJSON data loaded successfully:', data);
+        
+        // Function to calculate marker radius based on population
+        function calculateMarkerRadius(population) {
+            // Adjust the scale factor and exponent as needed
+            const scaleFactor = 0.01;
+            const exponent = 0.5;
+            return Math.pow(population, exponent) * scaleFactor;
+        }
 
+        // Iterate through the cities until a match is found
+        for (const city of data) {
+            const cityNameLC = cityName.toLowerCase();
+            // Check if the city name or ascii name matches the provided city name
+            if ((city.properties.name && city.properties.name.toLowerCase() === cityNameLC) ||
+                (city.properties.ascii_name && city.properties.ascii_name.toLowerCase() === cityNameLC) ||
+                (city.properties.alternate_names && // Check if alternate_names exists and is not null
+                city.properties.alternate_names.some(altName => altName.toLowerCase() === cityNameLC))) {
+                // Create circle markers for cities
+                const cityName = city.properties.name;
+                const cityCoordinates = city.geometry.coordinates.reverse(); // Reverse the coordinates for Leaflet
+                const cityPopulation = city.properties.population;
+
+                // Create a circle marker for the city with dynamic radius
+                const circleMarker = L.circleMarker(cityCoordinates, {
+                    radius: calculateMarkerRadius(cityPopulation),
+                    fillColor: 'red',
+                    color: 'black',
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                }).addTo(map);
+
+                // Create the content for the popup, including city name and population
+                const popupContent = `<b>${cityName}</b><br>Population: ${cityPopulation.toLocaleString()}`;
+
+                // Bind the popup with the content to the circle marker
+                circleMarker.bindPopup(popupContent);
+
+                // Call the callback with a truthy value to indicate success
+                callback(true);
+                return; // Exit the loop once a match is found
+            }
+        }
+        
+        // If no city is found, log a message and call the callback with a falsy value to indicate failure
+        console.log('City not found:', cityName);
+        callback(false);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON data:', error);
+        callback(false); // Call the callback with a falsy value to indicate failure
+    });
+}
+
+export { loadCities };
---
+++ leaflet_main/map.js
@@ -0,0 +1,120 @@
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var default_map_url = "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png";
+
+const input = document.getElementById('input');
+const search_button = document.getElementById('search_button');
+
+// Initialisiere die Leaflet-Karte
+var map = L.map('map', {
+    center: [51.1657, 10.4515], // Zentriere die Karte auf Deutschland
+    zoom: 6, // Passe den initialen Zoom-Level bei Bedarf an
+    maxBounds: [ // Definiere den maximalen Bereich der Karte
+        [90, -220], // Oben links: Nordpol, Westliche Hemisphäre
+        [-90, 220]  // Unten rechts: Südpol, Östliche Hemisphäre
+    ],
+    zoomControl: false,
+    layers: [
+        L.tileLayer(default_map_url, { // Verwende die bereitgestellte ArcGIS Online-Karte
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        })
+    ]
+});
+
+function addCustomZoomControl(map) {
+    // Erstelle eine neue Instanz der Zoom-Steuerung mit Optionen
+    var zoomControl = L.control.zoom({
+        position: 'bottomright' // Positioniere die Zoom-Steuerung unten rechts
+    });
+
+    // Füge die angepasste Zoom-Steuerung zur Karte hinzu
+    zoomControl.addTo(map);
+}
+
+
+addCustomZoomControl(map);
+
+import * as countriesFunctions from './countries.js';
+import * as citiesFunctions from './cities.js';
+
+countriesFunctions.loadCountries(map);
+
+function searchCity() {
+    // Get the value from the input field
+    const cityName = input.value;
+    console.log(cityName);
+    citiesFunctions.loadCities(map, cityName, function(success) {
+        if (success) {
+            input.value = "";
+        }
+    });
+}
+
+// Attach an event listener to the input element for keypress event
+input.addEventListener('keypress', function(event) {
+    // Check if the pressed key is the Enter key (key code 13)
+    if (event.keyCode === 13) {
+        // Call the searchCity function when Enter key is pressed
+        searchCity();
+    }
+});
+
+// Attach an event listener to the search button for click event
+search_button.addEventListener('click', function() {
+    // Call the searchCity function when the search button is clicked
+    searchCity();
+});
+
+
+// Attach an event listener to the input element
+input.addEventListener('input', function() {
+    // Call your function here
+    handleInputChange();
+});
+
+// Define the function to be called when the input changes
+function handleInputChange() {
+    // This function will be called whenever the input value changes
+    // You can perform any actions you want here
+    console.log('Input value changed');
+}
+
+// Function to set the tile layer
+function setTileLayer(layer) {
+    
+    console.log('Setting tile layer:', layer);
+    map.eachLayer(function (currentLayer) {
+        if (currentLayer instanceof L.TileLayer) {
+            console.log('Removing layer:', currentLayer);
+            map.removeLayer(currentLayer); // Remove basemap layers
+        }
+    });
+    if (layer) {
+        console.log('Adding new layer:', layer);
+        layer.addTo(map); // Add new layer if provided
+        layer.bringToBack();
+    }
+}
+
+// Add event listener to both radio buttons
+document.querySelectorAll('input[name="mapOption1"], input[name="mapOption2"]').forEach(function(radio) {
+    radio.addEventListener('change', function() {
+        console.log('Radio button changed:', this.value);
+        if (this.checked) {
+            if (this.value === 'NoMap') {
+                console.log('Selected: No Map');
+                setTileLayer(null); // Remove the basemap layer
+                // Uncheck the other radio button if it's checked
+                document.querySelector('input[name="mapOption2"][value="SatelliteMap"]').checked = false;
+            } else if (this.value === 'SatelliteMap') {
+                console.log('Selected: Satellite Map');
+                setTileLayer(Esri_WorldImagery); // Set the satellite map layer
+                // Uncheck the other radio button if it's checked
+                document.querySelector('input[name="mapOption1"][value="NoMap"]').checked = false;
+            }
+        }
+    });
+});
